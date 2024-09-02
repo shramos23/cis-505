abd22f7d43463f500f644df7d1f0b4c573331c16
shramos23 <132227546+shramos23@users.noreply.github.com>
2024-09-01 20:38:40 -0500

    RamosHelloWorld

--- Module_1/RamosHelloWorld.java
+++ Module_1/RamosHelloWorld.java
@@ -1,3 +1,9 @@
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modifications by S. Ramos 2024
+*/
+
 public class RamosHelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World");
