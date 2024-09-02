72dce6c5198953e98a51f89ce4843246d19d76d6
shramos23 <132227546+shramos23@users.noreply.github.com>
2024-09-01 20:32:04 -0500

    RamosHelloWorld

--- Module_1/RamosHelloWorld.java
+++ Module_1/RamosHelloWorld.java
@@ -1,6 +1,5 @@
-Public class Welcome {
-     public static void main (String[] args) {
-           // Display message Hello World On the console
-           System.out.printIn("Hello World!");
-      }
+public class RamosHelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
